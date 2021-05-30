da9b8c40090574e46b96431fe8361bc1fe356d39
LordabiX <okalay92@outlook.de>
2021-05-31 01:30:10 +0200

    js

---
+++ script/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+    window.alert('page is fully loaded');
+  });
+const page = {
+	Categories : [
+		{id: 'jackets', name: 'Jackets', imgURL: 'img/jackets06.jpg'},
+		{shirts: 'shirts', name: 'Shirts', imgURL: 'img/shirts06.jpg'},
+		{trousers: 'trousers', name: 'Trousers', imgURL: 'img/trousers07.jpg'},
+		{shoes: 'shoes', name: 'Shoes', imgURL: 'img/shoes07.jpg'},
+		{hats: 'hats', name: 'Hats', imgURL: 'img/hats08.jpg'},
+],
+
+}
